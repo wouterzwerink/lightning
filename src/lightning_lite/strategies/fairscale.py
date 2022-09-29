9d5a81c35d53a74619567cb1d2baae44eb7c5306
Adrian Wälchli <aedu.waelchli@gmail.com>
2022-09-29 14:01:34 +0200

    debug

--- src/lightning_lite/strategies/fairscale.py
+++ src/lightning_lite/strategies/fairscale.py
@@ -77,6 +77,8 @@ class DDPShardedStrategy(DDPStrategy):
             and a list of optimizer wrapped in :class:~`fairscale.optim.OSS`.
         """
         optimizers = _reinit_optimizers_with_oss(optimizers, self.precision_plugin, self.num_nodes)
+        for optimizer in optimizers:
+            optimizer._clear_cache()
         model = ShardedDataParallel(module, sharded_optimizer=optimizers, **self._ddp_kwargs)
         return model, optimizers
 
