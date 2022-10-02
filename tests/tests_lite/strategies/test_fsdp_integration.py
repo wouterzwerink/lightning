e0a47e950bc54f5c5d63ca4553179f3052864a54
Adrian Wälchli <aedu.waelchli@gmail.com>
2022-10-02 13:21:03 +0200

    debug

--- tests/tests_lite/strategies/test_fsdp_integration.py
+++ tests/tests_lite/strategies/test_fsdp_integration.py
@@ -60,12 +60,20 @@ class FSDPLite(BoringLite):
         super().run()
         with tempfile.TemporaryFile() as ckpt_path:
             ckpt_path = self.broadcast(str(ckpt_path))
+            self._strategy.save_checkpoint(self.model.state_dict(), ckpt_path)
 
-            checkpoint = dict(model=self.model.state_dict(), optimizer=self.optimizer.state_dict())
+        self._assert_save_equality(ckpt_path)
 
-            self._strategy.save_checkpoint(checkpoint, ckpt_path)
+    def _assert_save_equality(self, ckpt_path):
+        current_state_dict = self._strategy.get_module_state_dict(self.model)
 
-        _assert_save_equality(self, ckpt_path)
+        checkpoint = self.load(ckpt_path)
+        loaded_model = self.get_model()
+        loaded_model.load_state_dict(checkpoint)
+
+        # model parameters are identical after loading
+        for current_param, loaded_param in zip(current_state_dict.values(), loaded_model.state_dict().values()):
+            assert torch.equal(current_param.float().cpu(), loaded_param.cpu())
 
 
 @RunIf(min_cuda_gpus=1, skip_windows=True, standalone=True, min_torch="1.12")
@@ -74,15 +82,3 @@ def test_fsdp_train_save_load(precision):
     """Test to ensure that checkpoint is saved correctly when using a single GPU, and all stages can be run."""
     FSDPLite(accelerator="cuda", strategy="fsdp", devices=2, precision=precision).run()
 
-
-def _assert_save_equality(lite, ckpt_path):
-    model_state_dict = lite._strategy.get_module_state_dict(lite.model)
-
-    # if lite.is_global_zero:
-    checkpoint = lite.load(ckpt_path)
-    saved_model = lite.get_model()
-    saved_model.load_state_dict(checkpoint["model"])
-
-    # model parameters are identical after loading
-    for ddp_param, shard_param in zip(model_state_dict.values(), saved_model.state_dict().values()):
-        assert torch.equal(ddp_param.float().cpu(), shard_param.cpu())
